aaa43961ab489790318c18dcc1b2252136fe2056
henry@agnostik <henry@agnostik.io>
2020-02-06 15:56:37 +0100

    init

---
+++ src/components/InputBlock.vue
@@ -0,0 +1,33 @@
+<template>
+    <fieldset class="input--block">
+        <h3>
+            <slot></slot>
+        </h3>
+        <label for="truc">
+            <slot name="label"></slot>
+        </label>
+        <input name="truc"
+               v-bind:value="value"
+               v-bind:placeholder="placeholder"
+               v-on:input="$emit('input', $event.target.value)"
+        >
+        <span>
+            <slot name="message"></slot>
+        </span>
+    </fieldset>
+</template>
+
+<script>
+export default {
+  name: 'InputBlock',
+  props: {
+    value: String,
+    placeholder: String
+  }
+}
+</script>
+
+<style scoped="scoped">
+    .input--block > * {
+    }
+</style>
---
+++ src/views/Rightcol.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="aside">
+        <h1>Colonne de droite</h1>
+        <InputBlock v-bind:class="inputdemerde.class" v-bind:value="inputdemerde.value" v-on:input="inputdemerde.value = $event">
+            texte a mettre dans le Slot par défaut
+            <template v-slot:label>Texte dans le Slot Label</template>
+            <template v-slot:message>{{inputdemerde.message}}</template>
+        </InputBlock>
+        <hr/>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import InputBlock from '@/components/InputBlock.vue'
+
+export default {
+  name: 'rightcol',
+  components: {
+    InputBlock
+  },
+  data () {
+    return {
+      inputdemerde: {
+        value: '',
+        placeholder: 'placeholder de l\'input',
+        class: '',
+        message: 'message par défaut'
+      }
+    }
+  },
+  watch: {
+    'inputdemerde.value': function (val) {
+      if (val.length < 5) {
+        this.inputdemerde.class = 'error'
+        this.inputdemerde.message = 'trop court'
+      } else {
+        this.inputdemerde.class = ''
+        this.inputdemerde.message = 'ok !'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped="scoped">
+    .error {
+        background-color: #ff5555;
+    }
+</style>
